9e36122d432cc35ec6be2d97f3b3df3b6a9e0bf9
MwahMallah <maximus27rus@gmail.com>
2025-03-06 00:08:29 +0100

    Changed bot to use webhooks.

--- run.py
+++ run.py
@@ -1,44 +1,56 @@
 import asyncio
 import logging
-
-from aiogram import Bot, Dispatcher
-from routes.transactions import fin_router
-
 import os
 from dotenv import load_dotenv
 
-from flask import Flask
+from aiogram import Bot, Dispatcher
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 
-# Flask app
-app = Flask(__name__)
+from aiohttp import web
+from routes.transactions import fin_router
 
-@app.route('/')
-def home():
-    return "Bot is running!", 200
+# Load environment variables
+load_dotenv()
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+WEBHOOK_URL = os.getenv('WEBHOOK_URL') 
+WEBHOOK_PATH = "/webhook"  
 
-async def run_flask():
-    """Запускает Flask в фоновом потоке"""
-    loop = asyncio.get_running_loop()
-    await loop.run_in_executor(None, app.run, "0.0.0.0", 8080)
+# Bot and dispatcher initialization
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
 
-async def main():
-    # load token env variable for bot
-    load_dotenv()
+# Router registration
+dp.include_router(fin_router)
 
-    bot = Bot(token=os.getenv('BOT_TOKEN'))
-    dp = Dispatcher()
+# Create web application
+app = web.Application()
 
-    #includes finance router and starts polling    
-    dp.include_router(fin_router)
+# webhook setup
+async def on_startup(app):
+    await bot.set_webhook(url=f"{WEBHOOK_URL}{WEBHOOK_PATH}")
+    logging.info(f"Webhook: {WEBHOOK_URL}{WEBHOOK_PATH}")
 
-    # Create socket task
-    socket_task = asyncio.create_task(run_flask())
+# Close webhook on shutdown
+async def on_shutdown(app):
+    await bot.delete_webhook()
+    logging.info("Webhook delted")
 
-    # Create bot in separate task
-    bot_task = asyncio.create_task(dp.start_polling(bot))
-    
-    await asyncio.gather(socket_task, bot_task)
+# Registration of hooks
+app.on_startup.append(on_startup)
+app.on_shutdown.append(on_shutdown)
+
+# Webhook handler setup
+webhook_handler = SimpleRequestHandler(
+    dispatcher=dp,
+    bot=bot,
+)
+
+# Path setup
+webhook_handler.register(app, path=WEBHOOK_PATH)
+
+# aiogram integration
+setup_application(app, dp, bot=bot)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
-    asyncio.run(main())
+    web.run_app(app, host="0.0.0.0", port=8080)
